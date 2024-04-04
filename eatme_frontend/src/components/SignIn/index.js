e661f1b7022abdcab3bc099abacad6e70e847503
say1bt <sayuruneelika@gmail.com>
2024-04-04 09:37:36 +0530

    Added Navbar in SignIn Page

--- eatme_frontend/src/components/SignIn/index.js
+++ eatme_frontend/src/components/SignIn/index.js
@@ -1,24 +1,32 @@
 import React from "react";
 import getGoogleOAuthURL from "../../utils/getGoogleUrls";
+import Navbar from "../Navbar";
 
 const SignIn = () => {
   return (
-    <div className="signin-container">
-      <h2>Sign up or log in</h2>
-      <button className="signin-btn facebook">Continue with Facebook</button>
-      <button className="signin-btn google">
-        <a href={getGoogleOAuthURL()}>Continue with Google</a>
-      </button>
-      <button className="signin-btn apple">Continue with Apple</button>
-      <div className="separator">or</div>
-      <button className="signin-btn email">Continue with Email</button>
-      <p className="terms">
-        By continuing you agree to our <a href="#">T&Cs</a>. Please also check
-        out our <a href="#">Privacy Policy</a>. We use your data to offer you a
-        personalised experience and to better understand and improve our
-        services. For more information see <a href="#">here</a>.
-      </p>
-    </div>
+    <>
+      <Navbar />
+      <>
+        <div className="signin-container">
+          <h2>Sign up or log in</h2>
+          <button className="signin-btn facebook">
+            Continue with Facebook
+          </button>
+          <button className="signin-btn google">
+            <a href={getGoogleOAuthURL()}>Continue with Google</a>
+          </button>
+          <button className="signin-btn apple">Continue with Apple</button>
+          <div className="separator">or</div>
+          <button className="signin-btn email">Continue with Email</button>
+          <p className="terms">
+            By continuing you agree to our <a href="#">T&Cs</a>. Please also
+            check out our <a href="#">Privacy Policy</a>. We use your data to
+            offer you a personalised experience and to better understand and
+            improve our services. For more information see <a href="#">here</a>.
+          </p>
+        </div>
+      </>
+    </>
   );
 };
 
